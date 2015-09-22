7bda56020b3799ac46966f7fbce701f078d7e04c
Fuggle <whoiscatnow@gmail.com>
2015-09-22 22:27:39 +1000

    sound manager added

---
+++ Metamorph-o-tent/Assets/Demo/Scripts/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip soundLayer; //soundlayer 
+	AudioSource bgm;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//raise volume
+	public void raiseVolume () {
+		bgm = this.gameObject.GetComponent<AudioSource>();
+		bgm.volume = 1;
+	}
+
+	//lower volume
+	public void lowerVolume () {
+		bgm = this.gameObject.GetComponent<AudioSource>();
+		bgm.volume = 0;
+		Debug.Log(bgm.isPlaying);
+	}
+
+	//Add a new sound
+	public void addLayer () {
+		GameObject soundSource = new GameObject("soundLayerSource");
+		soundSource.AddComponent<AudioSource>();
+		AudioSource sfx = soundSource.GetComponent<AudioSource>();
+		sfx.PlayOneShot(soundLayer, 1);
+
+	}
+
+	void removeLayer () {
+	}
+
+}
