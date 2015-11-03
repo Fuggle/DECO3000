157d58ea6ff52df7694fbe6bbf7c51df6c19f889
David Chaseling <davidchaseling@live.com.au>
2015-11-03 16:13:06 +1000

    Experimental nodes added with new features

---
+++ Metamorph-o-tent/Assets/Rotate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float spinSpeed = 5;
+	bool isScalingUp = true;
+	int scaleBuffer = 0;
+	int limit = 100;
+	float scaleAmount = 0.002f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right * spinSpeed);
+
+		if (isScalingUp && scaleBuffer < limit) {
+			this.transform.localScale = new Vector3(transform.localScale.x + scaleAmount, transform.localScale.y + scaleAmount, transform.localScale.z + scaleAmount);
+			scaleBuffer++;
+			if (scaleBuffer == limit) {
+				isScalingUp = false;
+			}
+		}
+
+		if (!isScalingUp && scaleBuffer > -limit) {
+			this.transform.localScale = new Vector3(transform.localScale.x - scaleAmount, transform.localScale.y - scaleAmount, transform.localScale.z - scaleAmount);
+			scaleBuffer--;
+			if (scaleBuffer == -limit) {
+				isScalingUp = true;
+			}
+		}
+	}
+}
